f776129481b8dcb583be308aa8a4ede6a4726510
Marcel de Vries <marceldevries@my89.nl>
2012-06-13 17:05:15 +0200

    Initial commit

---
+++ BattleNET client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleNET_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
