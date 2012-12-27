603f5ed744b320be498464b0273c227c1fa6d19b
ziellos2k <marceldevries@my89.nl>
2012-12-27 16:09:47 +0100

    Prettier copyright notice.. ^^

--- BattleNET/BattlEyeClient.cs
+++ BattleNET/BattlEyeClient.cs
@@ -1,9 +1,9 @@
-﻿/*
- * BattleNET v1.1 - BattlEye Library and Client
- * 
- *  Copyright (C) 2012 by it's authors.
- *  Some rights reserverd. See COPYING.TXT, AUTHORS.TXT.
- */
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * BattleNET v1.1 - BattlEye Library and Client            *
+ *                                                         *
+ *  Copyright (C) 2012 by it's authors.                    *
+ *  Some rights reserverd. See COPYING.TXT, AUTHORS.TXT.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 using System;
 using System.Collections.Generic;
