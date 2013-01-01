fb5019ffa54cdfd7967918826fe6d8aa22e2f259
ziellos2k <marceldevries@my89.nl>
2013-01-01 21:25:57 +0100

    Happy new year! :)

--- BattleNET/CRC32.cs
+++ BattleNET/CRC32.cs
@@ -1,7 +1,7 @@
 ﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * BattleNET v1.2 - BattlEye Library and Client            *
  *                                                         *
- *  Copyright (C) 2012 by it's authors.                    *
+ *  Copyright (C) 2013 by it's authors.                    *
  *  Some rights reserved. See COPYING.TXT, AUTHORS.TXT.    *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
