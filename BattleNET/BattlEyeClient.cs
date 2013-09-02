6fe49d93dd74611c606f5c9397ce461df5b4d693
ziellos2k <marceldevries@my89.nl>
2013-09-02 21:24:13 +0200

    Increase packet timeout time (2 -> 5)

--- BattleNET/BattlEyeClient.cs
+++ BattleNET/BattlEyeClient.cs
@@ -322,7 +322,7 @@ namespace BattleNET
                             DateTime date = DateTime.Parse(packetQueue[key][1]);
                             int timeDiff = (int)(DateTime.Now - date).TotalSeconds;
 
-                            if (timeDiff > 2)
+                            if (timeDiff > 5)
                             {
                                 SendCommandPacket(value, false);
                                 packetQueue.Remove(key);
