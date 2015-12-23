f2b791fd849035f5cf5d0a923e0a790882ff07f4
Marcel de Vries <marceldev89@gmail.com>
2015-12-23 22:01:47 +0100

    Update changelog and version numbers

--- BattleNET/BattlEyeDisconnectEventArgs.cs
+++ BattleNET/BattlEyeDisconnectEventArgs.cs
@@ -1,5 +1,5 @@
 ﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * BattleNET v1.3.2 - BattlEye Library and Client            *
+ * BattleNET v1.3.3 - BattlEye Library and Client            *
  *                                                         *
  *  Copyright (C) 2015 by it's authors.                    *
  *  Some rights reserved. See license.txt, authors.txt.    *
